5ec385fb2b64ff9dfa4bdc0d5ae94692b6ed4938
Ahmad Rifki Ayala <ayalarifki@gmail.com>
2024-09-05 09:45:35 +0700

    update func btn login for generate cookie redirect

--- js/btn-login.js
+++ js/btn-login.js
@@ -2,27 +2,30 @@ import { onClick } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/element.j
 import { redirect } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/url.js';
 
 onClick('loginulbi', loginUlbi);
+
 function loginUlbi() {
   console.log('loginulbi');
-  setCookieWithExpireHour('redirect', window.location.href, 1);
-    redirect('https://login.ulbi.ac.id');
 
-    setTimeout(() => {
-      redirect('https://login.ulbi.ac.id');
-    }, 100);
+  // Menyimpan URL saat ini ke dalam cookie sebelum redirect
+  setCookieWithExpireHour('redirect', window.location.href, 1);
+
+  // Menambahkan sedikit delay untuk memastikan cookie tersimpan sebelum redirect
+  setTimeout(() => {
+    redirect('https://login.ulbi.ac.id');
+  }, 100);
 }
 
 export function setCookieWithExpireHour(cname, cvalue, exhour) {
     const d = new Date();
     d.setTime(d.getTime() + (exhour * 60 * 60 * 1000));
     let expires = "expires=" + d.toUTCString();
-    
+
     // Set domain to ulbi.ac.id to allow subdomains access
     let domain = "domain=.ulbi.ac.id"; 
 
-    let samSite = "SameSite=None; Secure";
+    // Set SameSite and Secure for cross-subdomain cookie sharing (especially for HTTPS)
+    let sameSite = "SameSite=None; Secure";
   
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";" + domain + ";path=/" + samSite;
-  }
-  
-  
+    // Menyimpan cookie dengan atribut yang benar
+    document.cookie = cname + "=" + encodeURIComponent(cvalue) + ";" + expires + ";" + domain + ";path=/;" + sameSite;
+}
