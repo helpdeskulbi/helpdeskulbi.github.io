36f01208e99d1868a900b0cc2f2333398d2167d9
GilangAndhika <gryzlegrizz@gmail.com>
2024-09-23 10:44:52 +0700

    Add login button functionality and refactor cookie handling

--- js/btn-login.js
+++ js/btn-login.js
@@ -1,29 +1,26 @@
 import { onClick } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/element.js";
 import { redirect } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/url.js";
 
-onClick('loginulbi', loginUlbi);
-
-function loginUlbi() {
-    console.log('loginulbi');
-    
-    // Menyimpan URL saat ini ke dalam cookie
-    setCookieWithExpireHour('redirect', window.location.href, 1);
-    
-    // Tambahkan logging untuk memastikan URL tersimpan
-    console.log('Current href:', window.location.href);
-    
-    // Redirect ke halaman login
-    redirect('https://login.ulbi.ac.id');
+function onClick(id, actionfunctionname) {
+    document.getElementById(id).onclick = actionfunctionname;
 }
 
 function setCookieWithExpireHour(cname, cvalue, exhour) {
     const d = new Date();
     d.setTime(d.getTime() + (exhour * 60 * 60 * 1000));
     let expires = "expires=" + d.toUTCString();
-    
+
     // Set domain to ulbi.ac.id to allow subdomains access
-    let domain = "domain=.ulbi.ac.id"; 
-  
-    // Set the cookie with name, value, expiry, domain, and path
-    document.cookie = cname + "=" + encodeURIComponent(cvalue) + ";" + expires + ";" + domain + ";path=/";
+    let domain = "domain=.ulbi.ac.id";
+
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";" + domain + ";path=/";
 }
+
+
+function loginUlbi() {
+    console.log('loginulbi');
+    setCookieWithExpireHour('redirect', window.location.href, 1);
+    console.log('url yang disimpam:', window.location.href);
+}
+
+onClick('loginulbi', loginUlbi);
