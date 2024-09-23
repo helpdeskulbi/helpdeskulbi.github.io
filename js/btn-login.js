49510f1361957dd897cc44768005cf9db720faa0
GilangAndhika <gryzlegrizz@gmail.com>
2024-09-23 10:49:55 +0700

    Refactor login button functionality and improve cookie handling

--- js/btn-login.js
+++ js/btn-login.js
@@ -1,10 +1,6 @@
 import { onClick } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/element.js";
 import { redirect } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/url.js";
 
-function onClick(id, actionfunctionname) {
-    document.getElementById(id).onclick = actionfunctionname;
-}
-
 function setCookieWithExpireHour(cname, cvalue, exhour) {
     const d = new Date();
     d.setTime(d.getTime() + (exhour * 60 * 60 * 1000));
@@ -16,11 +12,11 @@ function setCookieWithExpireHour(cname, cvalue, exhour) {
     document.cookie = cname + "=" + cvalue + ";" + expires + ";" + domain + ";path=/";
 }
 
-
 function loginUlbi() {
     console.log('loginulbi');
     setCookieWithExpireHour('redirect', window.location.href, 1);
-    console.log('url yang disimpam:', window.location.href);
+    console.log('url yang disimpan:', window.location.href);
 }
 
+// Menggunakan onClick dari library yang diimpor
 onClick('loginulbi', loginUlbi);
